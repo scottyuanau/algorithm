bd7dce481654d7b7b121f287d2f8dcd1e6dfc721
Scott Yuan <scoworthyuan@gmail.com>
2023-01-05 19:13:38 +1100

    optimized merge sort to avoid the shift method

--- merge-sort.js
+++ merge-sort.js
@@ -1,14 +1,43 @@
-function merge(leftArr, rightArr) {
-    let resultArr=[];
-    while (leftArr.length && rightArr.length) {
-        if(leftArr[0]>=rightArr[0]){
-            resultArr.push(rightArr.shift());
-        } else {
-            resultArr.push(leftArr.shift());
-        }
+//this version uses the shift method, which is O(N2) operation.
+// function merge(leftArr, rightArr) {
+//     let resultArr=[];
+//     while (leftArr.length && rightArr.length) {
+//         if(leftArr[0]>=rightArr[0]){
+//             resultArr.push(rightArr.shift());
+//         } else {
+//             resultArr.push(leftArr.shift());
+//         }
 
+//     }
+//     return [...resultArr,...leftArr,...rightArr];
+// }
+
+//the following code avoids shift method, which is O(N) operation.
+function merge(leftArr, rightArr) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < leftArr.length && rightIndex < rightArr.length)
+  if (leftArr[leftIndex] <= rightArr[rightIndex]){
+    result.push(leftArr[leftIndex])
+    leftIndex += 1
+  } else {
+    result.push(rightArr[rightIndex])
+    rightIndex += 1
+  }
+  //push remainings into the result if right arr has left over
+  if(leftIndex == leftArr.length && rightIndex < rightArr.length) {
+    for(let i = rightIndex; i < rightArr.length; i++) {
+      result.push(rightArr[i])
     }
-    return [...resultArr,...leftArr,...rightArr];
+  }
+  //push remainings into the result if left arr has left over
+  if(rightIndex == rightArr.length && leftIndex < leftArr.length) {
+    for(let i = leftIndex; i < leftArr.length; i++) {
+      result.push(leftArr[i])
+    }
+  }
+  return result;
 }
 
 function mergeSort(arr) {
