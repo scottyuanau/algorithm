ee386f22dc3054f30ed569a1f1f12f35d995b52a
Scott Yuan <scoworthyuan@gmail.com>
2023-01-12 07:54:33 +1100

    Create Powerset.js

---
+++ Recursion/Powerset.js
@@ -0,0 +1,26 @@
+/*
+Powerset
+Write a function that takes in an array of unique integers and returns its powerset.
+The powerset P(X) of a set X is the set of all subsets of X . For example, the powerset of [1,2] is [[], [1], [2], [1,2]] .
+Note that the sets in the powerset do not need to be in any particular order.
+Sample Input
+array = [1, 2, 3] 
+
+Sample Output
+[[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+*/
+
+
+//O(n*2^n) Time, O(n*2^n)space. N is the length of the input array
+function powerset(array) {
+  // Write your code here.
+  let result = [[]]
+  array.map((number)=>{
+    result.map((subArr)=>{
+      let newArr = subArr.concat(number) 
+      //concat method doesn't mutate original array
+      result.push(newArr)
+    })
+  })
+  return result
+}
